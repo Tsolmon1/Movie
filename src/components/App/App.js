c6af57a777f85626bdc4d8502b7660dd476bb6e3
tsolmon <b.tsoom0116@gmail.com>
2020-08-09 16:57:38 +0800

    route fix

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,7 +11,7 @@ const App = () => {
       <React.Fragment>
         <Header />
         <Switch>
-          <Route path="/Movie" component={Home} exact />
+          <Route path="/" component={Home} exact />
           <Route path="/:movieId" component={Movie} exact />
           <Route component={NotFound} />
         </Switch>
