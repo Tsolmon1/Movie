a11c7d401a2a3625e020ca6a8e96101c54080228
tsolmon <b.tsoom0116@gmail.com>
2020-08-09 16:17:39 +0800

    route fix

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch} from 'react-router-dom';
-import Header from '../elements/Header/Header';
-import Home from '../Home/Home';
-import Movie from '../Movie/Movie';
-import NotFound from '../elements/NotFound/NotFound';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Header from "../elements/Header/Header";
+import Home from "../Home/Home";
+import Movie from "../Movie/Movie";
+import NotFound from "../elements/NotFound/NotFound";
 
 const App = () => {
   return (
@@ -11,13 +11,13 @@ const App = () => {
       <React.Fragment>
         <Header />
         <Switch>
-          <Route path="/" component={Home} exact />
-          <Route path="/:movieId" component={Movie} exact />
+          <Route path="/Movie/" component={Home} exact />
+          <Route path="/Movie/:movieId" component={Movie} exact />
           <Route component={NotFound} />
         </Switch>
       </React.Fragment>
     </BrowserRouter>
-  )
-}
+  );
+};
 
 export default App;
